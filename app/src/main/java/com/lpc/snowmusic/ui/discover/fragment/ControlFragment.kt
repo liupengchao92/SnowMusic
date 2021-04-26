e9a1294b15b054855fc78064413c016f6d143d09
liupengchao <876367352@qq.com>
2021-04-26 19:09:09 +0800

    播放音乐控制栏

---
+++ app/src/main/java/com/lpc/snowmusic/ui/discover/fragment/ControlFragment.kt
@@ -0,0 +1,24 @@
+package com.lpc.snowmusic.ui.discover.fragment
+
+import android.view.View
+import com.lpc.snowmusic.R
+import com.lpc.snowmusic.base.BaseFragment
+
+/**
+ * Author: liupengchao
+ * Date: 2021/4/26
+ * ClassName :ControlFragment
+ * Desc:
+ */
+class ControlFragment : BaseFragment() {
+
+    override fun getLayoutResId(): Int = R.layout.fragment_control_layout
+
+    override fun initView(view: View) {
+
+    }
+
+    override fun lazyLoad() {
+
+    }
+}
