26e69f217761221d10c9b39c922338a74a50d086
LPC <876367352@qq.com>
2021-06-03 15:03:50 +0800

    本地播放

--- app/src/main/java/com/lpc/snowmusic/ui/my/activity/PlayLocalActivity.kt
+++ app/src/main/java/com/lpc/snowmusic/ui/my/activity/PlayLocalActivity.kt
@@ -1,12 +1,14 @@
 package com.lpc.snowmusic.ui.my.activity
 
-import com.blankj.utilcode.util.LogUtils
+import android.content.Context
 import com.lpc.snowmusic.R
 import com.lpc.snowmusic.base.BaseMvpActivity
-import com.lpc.snowmusic.database.loader.PlayLocalLoader
 import com.lpc.snowmusic.mvp.contract.PlayLocalContract
 import com.lpc.snowmusic.mvp.presenter.PlayLocalPresenter
-import org.jetbrains.anko.doAsync
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView
 
 class PlayLocalActivity : BaseMvpActivity<PlayLocalContract.View, PlayLocalContract.Presenter>() {
 
@@ -25,9 +27,20 @@ class PlayLocalActivity : BaseMvpActivity<PlayLocalContract.View, PlayLocalContr
 
     override fun initView() {
         super.initView()
-        LogUtils.d("标题：${toolBar.title}")
-        doAsync {
-            PlayLocalLoader.getLocalMusic(this@PlayLocalActivity)
+        val navigator: CommonNavigator = CommonNavigator(this)
+        navigator.adapter=object : CommonNavigatorAdapter() {
+
+            override fun getCount(): Int {
+                TODO("Not yet implemented")
+            }
+
+            override fun getTitleView(p0: Context?, p1: Int): IPagerTitleView {
+                TODO("Not yet implemented")
+            }
+
+            override fun getIndicator(p0: Context?): IPagerIndicator {
+                TODO("Not yet implemented")
+            }
         }
     }
 
